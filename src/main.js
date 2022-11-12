4d9aff98b75ce5b5ef34ba57545c0853fe8c6c74
Jules Pognante <pognante.jules@gmail.com>
2022-11-12 19:34:28 +0100

    implemented bootstrap design in website except for destinations page

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ let defaultDestinations = [
 
 // Constant variable which contains the html code of the index page
 const index = `
-    <div class="ms-4 mt-4">
+    <div class="m-4">
     <h2>Description générale</h2>
     <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, dicta? Quae quisquam, consequuntur veritatis suscipit quidem, natus optio aut adipisci cupiditate, fuga eveniet! Neque nemo dolorem aut aliquid corrupti quos.</blockquote>
     <p>
@@ -69,9 +69,10 @@ const destinations = `
 
 // Constant variable which contains the html code of the Espace perso/Mes informations page
 const perso = `
-    <div class="card">
-        <h1>Espace perso</h1>
-        <ul>
+    <div class="card mt-5" style="width: 25rem; height:20rem">
+        <div class="card-body d-flex justify-content-center flex-column align-items-center">
+        <h3 class="card-title mb-4">Espace perso</h3>
+        <ul class="card-text">
             <li>Connexion</li>
             <li>Mes informations
                 <ul>
@@ -83,15 +84,17 @@ const perso = `
             <li>Messagerie</li>
             <li>Historique</li>
         </ul>
+        </div>
     </div>
 `;
 
 // Constant variable which contains the html code of the Voyage virtuel audio page
 const voyage_audio = `
+    <div class="mt-5">
     <div>
         <h2>Sons de crickets</h2>
         <audio controls autoplay muted>
-            <source src="medias/audio/crickets-By-Tuna.mp3" type="medias/audio/mp3"></audio>
+            <source src="medias/audio/crickets-By-Tuna.mp3"></audio>
         </audio> 
     </div>
     <hr>
@@ -109,60 +112,77 @@ const voyage_audio = `
         <h2>Son d'ambiance plage</h2>
         <a href="medias/audio/oceanwave.mp3">Ecouter</a>
     </div>
+    </div>
 `;
 
 // Constant variable which contains the html code of the Voyage virtuel video page
 const voyage_video = `
+    <div class="m-4">
     <h3>Indonésie</h3>
     <video controls autoplay width="320px" height="240px" muted>
         <source src="medias/video/ind.mp4" type="video/mp4">
     </video> 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, consequuntur, corrupti iure temporibus rem voluptatem accusamus amet reprehenderit, laudantium minima sequi in! Dignissimos sint, accusantium iure et, ipsam distinctio necessitatibus recusandae suscipit aliquam hic cum fuga omnis! Labore, nesciunt sunt dolor cumque optio repellat illum magni obcaecati sequi in fugiat quae velit provident enim iste omnis commodi nemo officiis suscipit dolores incidunt repellendus laudantium! Iure nam rem rerum est dolores error beatae doloremque alias eum veritatis qui autem quod enim suscipit ipsum, a placeat iste, sed hic eius ea. Possimus numquam modi minus maiores dolor laboriosam esse, tempora nihil nostrum, officiis excepturi corporis exercitationem iste commodi ipsum soluta. Quia corporis vitae, ratione natus quasi expedita quo rerum totam ex provident officia ut illum quibusdam sunt minima qui deleniti aliquam voluptas magnam architecto quidem facilis et recusandae explicabo! Quibusdam amet accusantium fugit minus rerum quos consequatur sit maxime dolor numquam! Modi nostrum sunt at aliquam, illum quod harum itaque. Reprehenderit corrupti sequi optio in incidunt veniam impedit! Perspiciatis dolore itaque maiores dolores commodi quos pariatur nisi voluptatibus, rerum voluptas, vero deleniti debitis quo tempora optio unde vel veritatis magni dicta harum laboriosam porro perferendis consectetur a. Quidem ab sequi alias pariatur! Repellat excepturi omnis quae qui, quia dolorum. Nostrum recusandae magni illum unde quidem minima quibusdam eveniet qui exercitationem facere. Facere, sequi! Magnam molestiae tenetur blanditiis? Magni excepturi praesentium error hic, odit qui illum dolorem.</p>
+    </div>
+    <div class="m-4">
     <h3>Indonésie</h3>
     <video controls width="320px" height="240px" >
         <source src="medias/video/swz.mp4" type="video/mp4">
     </video> 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, consequuntur, corrupti iure temporibus rem voluptatem accusamus amet reprehenderit, laudantium minima sequi in! Dignissimos sint, accusantium iure et, ipsam distinctio necessitatibus recusandae suscipit aliquam hic cum fuga omnis! Labore, nesciunt sunt dolor cumque optio repellat illum magni obcaecati sequi in fugiat quae velit provident enim iste omnis commodi nemo officiis suscipit dolores incidunt repellendus laudantium! Iure nam rem rerum est dolores error beatae doloremque alias eum veritatis qui autem quod enim suscipit ipsum, a placeat iste, sed hic eius ea. Possimus numquam modi minus maiores dolor laboriosam esse, tempora nihil nostrum, officiis excepturi corporis exercitationem iste commodi ipsum soluta. Quia corporis vitae, ratione natus quasi expedita quo rerum totam ex provident officia ut illum quibusdam sunt minima qui deleniti aliquam voluptas magnam architecto quidem facilis et recusandae explicabo! Quibusdam amet accusantium fugit minus rerum quos consequatur sit maxime dolor numquam! Modi nostrum sunt at aliquam, illum quod harum itaque. Reprehenderit corrupti sequi optio in incidunt veniam impedit! Perspiciatis dolore itaque maiores dolores commodi quos pariatur nisi voluptatibus, rerum voluptas, vero deleniti debitis quo tempora optio unde vel veritatis magni dicta harum laboriosam porro perferendis consectetur a. Quidem ab sequi alias pariatur! Repellat excepturi omnis quae qui, quia dolorum. Nostrum recusandae magni illum unde quidem minima quibusdam eveniet qui exercitationem facere. Facere, sequi! Magnam molestiae tenetur blanditiis? Magni excepturi praesentium error hic, odit qui illum dolorem.</p>
+    </div>
 
 `;
 
 // Constant variable which contains the html code of the Contact page
 const contact = `
-    <form method="post">
-        <p>
-            <label for="nom">Nom et Prénom</label>
-            <input type="text" id="nom" required>
-        </p>
-        <p>
-            <label for="email">Adresse mail</label>
-            <input type="email" id="email" required>
-        </p>    
-        <p>
-            <label for="tel">Numéro de téléphone</label>
-            <input type="tel" id="tel" pattern="[0]{1}[0-9]{9}" required>
-        </p>
-        <p>
-            <label for="msg">Message</label>
-            <input type="text" id="msg" required>
-        </p>
-        <button>Envoyer</button>
+    <div class="card mt-4" style="width:25rem;">
+    <form class="card-body" method="post">
+        <div class="mb-3">
+            <label class="form-label" for="nom">Nom et Prénom</label>
+            <input class="form-control" type="text" id="nom" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="email">Adresse mail</label>
+            <input class="form-control" type="email" id="email" required>
+        </div>    
+        <div class="mb-3">
+            <label class="form-label" for="tel">Numéro de téléphone</label>
+            <input class="form-control" type="tel" id="tel" pattern="[0]{1}[0-9]{9}" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="msg">Message</label>
+            <input class="form-control" type="text" id="msg" required>
+        </div>
+        <button type="submit" class="btn btn-outline-primary">Envoyer</button>
     </form>
+    </div>
 `
 
 // Constant variable which contains the html code of the Nouvelle destinations/modification d'une destination page
 const destInfos = `
-    <form method="post" class="formEvent">
-        <label for="destination">Destination : </label><br>
-        <input type="text" id="destination" name="destination" required><br>
-        <label for="photo">Photo : </label><br>
-        <img id="imgChoosed" src=""/><br>
-        <input onchange="showImage()" type="file" id="photo" name="photo"><br>
-        <label for="circuit">Circuit (séparer les activités par des virgules) : </label><br>
-        <textarea id="circuit" name="circuit" required placeholder="Activité1,Activité2,Activité3,..."></textarea><br>
-        <label for="tarif">Prix : </label><br>
-        <input type="number" id="tarif" name="tarif" required><br>
-        <input id="submitDestInfos" type="submit">
+    <div class="card mt-4" style="width:30rem;">
+    <form class="card-body" method="post" id="formEvent">
+        <div class="mb-3">
+            <label class="form-label" for="destination">Destination : </label><br>
+            <input class="form-control" type="text" id="destination" name="destination" required><br>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="photo">Photo : </label><br>
+            <img class="mb-2" id="imgChoosed" src=""/><br>
+            <input class="form-control" onchange="showImage()" type="file" id="photo" name="photo"><br>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="circuit">Circuit (séparer les activités par des virgules) : </label><br>
+            <textarea class="form-control" id="circuit" name="circuit" required placeholder="Activité1,Activité2,Activité3,..."></textarea><br>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="tarif">Prix : </label><br>
+            <input class="form-control" type="number" id="tarif" name="tarif" required><br>
+        </div>
+        <button class="btn btn-outline-primary" id="submitDestInfos" type="submit"></button>
     </form>
+    </div>
 `
 
 // Sets the default page displayed to 'index' page, we have to use window.onload to be able to find the element with
@@ -193,9 +213,9 @@ function readFile(file){
 // Sets the current page to the destInfos page and sets the registering of a destination's info into a new destination object
 function newDest(){
     switchPage('destInfos');
-    let form = document.getElementsByClassName('formEvent')[0];
+    let form = document.getElementById('formEvent');
     document.getElementById('photo').required = true;
-    document.getElementById('submitDestInfos').value = "Créer";
+    document.getElementById('submitDestInfos').innerText = "Créer";
     form.addEventListener('submit', async (event) => {
         // we do event.preventDefault to prevent the page to reload when the user sends the form
         event.preventDefault();
@@ -211,7 +231,7 @@ function newDest(){
         defaultDestinations.push(newDest);
 
         //after creating the new destination, we push the user back to the destination page
-        switchPage('destinations');
+        setupDestinations();
     })
 }
 
@@ -220,13 +240,13 @@ function delDest(id){
     // we filter the destinations list to only keep the destinations with an id different from the one
     // the user wants to delete
     defaultDestinations = defaultDestinations.filter(dest => dest.id!=id);
-    switchPage('destinations');
+    setupDestinations();
 }
 
 // Displays the destInfos page and loads all the informations of the destination the user wants to edit
 function editDest(id){
     switchPage('destInfos');
-    document.getElementById('submitDestInfos').value = "Modifier"
+    document.getElementById('submitDestInfos').innerText = "Modifier"
     let destToEdit = defaultDestinations[defaultDestinations.findIndex(value => value.getId == id)];
     // we set the required attribute to false for the picture because when we load the already existing picture,
     // it's nnot possible to set it in the file input
@@ -235,8 +255,8 @@ function editDest(id){
     document.getElementById('imgChoosed').src    = destToEdit.getPhoto;
     document.getElementById('circuit').value     = destToEdit.getCircuit.join(',');
     document.getElementById('tarif').value       = destToEdit.getTarif;
-    document.getElementsByClassName('formEvent')[0].addEventListener('submit', async (event) => {
-        let form = document.getElementsByClassName('formEvent')[0];
+    document.getElementById('formEvent').addEventListener('submit', async (event) => {
+        let form = document.getElementById('formEvent');
         event.preventDefault();
         let circuitTmp = form.elements["circuit"].value;
         console.log(circuitTmp);
@@ -251,7 +271,7 @@ function editDest(id){
         destToEdit.setCircuit = circuitFiltered;
         destToEdit.setDestination = form.elements['destination'].value;
         destToEdit.setTarif = form.elements['tarif'].value;
-        switchPage('destinations');
+        setupDestinations();
     });
 
 }
